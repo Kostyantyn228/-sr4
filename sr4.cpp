2ac8c06e9945431cadb24bf8e5085b2c0434d428
Kostyantyn228 <terfas566@gmail.com>
2024-11-15 08:10:21 +0000

    Finaly version

--- sr4.cpp
+++ sr4.cpp
@@ -8,3 +8,12 @@ int main() {
 
     cout << "Введіть ціле число n і дійсне число x: ";
     cin >> n >> x;
+    
+    for (int i = 1; i <= n; i++) {
+        u = pow(-1, i) * (2 * i - 1) / pow(x, i);
+        s += u;
+    }
+
+    cout << "Результат обчислення s: " << s << endl;
+    return 0;
+}
