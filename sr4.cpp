74cbc3c89dfe418090fb1949c341d80283ab34fd
Kostyantyn228 <terfas566@gmail.com>
2024-11-15 08:09:33 +0000

    First version

--- sr4.cpp
+++ sr4.cpp
@@ -1,1 +1,10 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
 
+int main() {
+    int n;
+    double x, u, s = 0.0;
+
+    cout << "Введіть ціле число n і дійсне число x: ";
+    cin >> n >> x;
